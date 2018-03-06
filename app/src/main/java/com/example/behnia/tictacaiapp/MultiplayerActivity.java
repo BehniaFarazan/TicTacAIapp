fda242f46ae411a3a535d6e03b7ce239932e402a
BehniaFarazan <b.farazan@gmail.com>
2018-03-06 20:48:04 +0100

    commit

--- app/src/main/java/com/example/behnia/tictacaiapp/MultiplayerActivity.java
+++ app/src/main/java/com/example/behnia/tictacaiapp/MultiplayerActivity.java
@@ -1,14 +1,69 @@
 package com.example.behnia.tictacaiapp;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MultiplayerActivity extends AppCompatActivity {
+public class MultiplayerActivity extends AppCompatActivity implements View.OnClickListener {
+
     private final MultiplayerLogic mylogic = MultiplayerLogic.getInstance();
+
+    private final Button[][] btnArray = new Button[3][3];
+
+    int[][] btnIdArray = {{R.id.button_00,R.id.button_01,R.id.button_02},{R.id.button_10,R.id.button_11,R.id.button_12},{R.id.button_20,R.id.button_21,R.id.button_22}};
+
+    public String getBtnArray() {
+        return btnArray.toString();
+    }
+
+boolean p1turn = true ;
+    boolean p2turn = true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_multiplayer);
+
+        mylogic.roundStart();
+
+        for (int i = 0; i < btnArray.length; i++) {
+            for (int j = 0; j < btnArray[0].length; j++) {
+            btnArray[i][j] = (Button) findViewById(btnIdArray[i][j]);
+            btnArray[i][j].setOnClickListener(this);
+        }
+        }
+    }
+    @Override
+    public void onClick(View view) {
+
+        if(p1turn){
+        if (view instanceof Button) {
+            view.setEnabled(false);
+            ((Button) view).setTextColor(Color.GREEN);
+            ((Button) view).setText("O");
+            p1turn=false;
+            p2turn=true;
+        }}
+
+        else if(p2turn){
+            if (view instanceof Button) {
+                view.setEnabled(false);
+                ((Button) view).setTextColor(Color.RED);
+                ((Button) view).setText("X");
+                p2turn=false;
+                p1turn=true;
+            }}
+          
+            //mylogic.cheackWin();
+    }
+
+
+
+
+
+
+    MultiplayerActivity(){
 
     }
 }
--- app/src/main/java/com/example/behnia/tictacaiapp/MultiplayerLogic.java
+++ app/src/main/java/com/example/behnia/tictacaiapp/MultiplayerLogic.java
@@ -1,19 +1,30 @@
 package com.example.behnia.tictacaiapp;
 
+import android.view.View;
+import android.widget.Button;
+
 /**
  * Created by Behnia on 06-03-18.
  */
 
 public class MultiplayerLogic {
     private static MultiplayerLogic instance;
+    MultiplayerActivity ma = new MultiplayerActivity();
+    String x = "X";
+    String o = "O";
 private Boolean p1turn,p2turn;
     private int round;
     MultiplayerLogic(){
         start();
         roundStart();
-
     }
 
+    public Boolean getP1turn() {
+        return p1turn;
+    }
+    public Boolean getP2turn() {
+        return p2turn;
+    }
 
     static {
         try {
@@ -33,10 +44,34 @@ private Boolean p1turn,p2turn;
 
     private void start() {
         p1turn=true;
-        p2turn=false;
+        p2turn=true;
     }
-    private void roundStart() {
+    void pOneTurn()
+    {
+pOneChoose();
+    }
+    void pTwoTurn()
+    {
+pTwoChoose();
+    }
+   void pOneChoose(){
+
+   }
+    void pTwoChoose(){
+
+    }
+    public void roundStart() {
         roundReset();
+        do {
+            round++;
+            //pOneTurn();
+            //pTwoTurn();
+        }while (round==3);
     }
 
-}
+    public void cheackWin(View view) {
+       // if(ma.btnIdArray[0][0]=x&& ma.btnIdArray[0][1]= x){
+
+        }
+    }
+
